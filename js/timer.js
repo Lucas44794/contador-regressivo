5facc019db557b5a2b47aa2a177632fa594f3caa
Lucas Rogerio da Silva <109930651+Lucas44794@users.noreply.github.com>
2022-10-03 23:48:07 -0300

    adicionando comentarios

--- js/timer.js
+++ js/timer.js
@@ -1,8 +1,8 @@
+//Variaveis
 var display = document.getElementById("timer-tempo");
 var horas = document.getElementById("horas");
 var minutos = document.getElementById("minutos");
 var segundos = document.getElementById("segundos");
-
 var iniciar = document.getElementById("iniciar--pausar");
 var cancelar = document.getElementById("cancelar")
 
@@ -10,7 +10,6 @@ var cancelar = document.getElementById("cancelar")
 var horaAtual;
 var minutoAtual;
 var segundoAtual;
-
 var interval;
 
 for(var i = 0; i<=60; i++){
@@ -22,6 +21,8 @@ for(var i = 0; i<=60; i++){
 for(var i = 0; i<=60; i++){
     segundos.innerHTML += '<option value='+i+'>'+i+'</option>';
 }
+
+//função que faz o calculo no timer sempre diminuindo de segundo em segundo
 function calculo (){
     iniciar.innerHTML = "Pausar"
     segundoAtual--;
@@ -53,6 +54,8 @@ function calculo (){
     display.innerHTML = horaAtual+":"+minutoAtual+":"+segundoAtual
 }
 
+//Adicionando o evento iniciar e pausar no painel, fazendo com que o calculo inicie e pare respectivamente
+//variando dependendo da string exibida no painel
 iniciar.addEventListener('click', function() {
     if (this.innerHTML == "Pausar") {
         this.innerHTML = "Iniciar "
@@ -78,6 +81,7 @@ iniciar.addEventListener('click', function() {
 
 })
 
+//Adicionando ao botão cancelar a função de parar o timer e zerar a contagem
 cancelar.addEventListener('click', function(){
     clearInterval(interval)
     horaAtual = "00"
@@ -90,13 +94,14 @@ cancelar.addEventListener('click', function(){
 
 })
 
+//Configura a exibição no painel, acrescentando um 0 caso não sejam dois digitos via string
 function horaminuto (){
     horaAtual = horaAtual.toString().padStart(2,"0");
     minutoAtual = minutoAtual.toString().padStart(2,"0");
     segundoAtual = segundoAtual.toString().padStart(2,"0");
 }
 
-
+//Adiciona eventos nos botões do teclado
 document.addEventListener("keydown", function(e){
 	if (e.keyCode == 73 ){
         horaAtual = horas.value
